2164940c49dcd31b15d1d45cd384662a0135adf4
Raghu Raghavan <dadra.raghu@live.com>
2019-01-25 23:12:08 -0800

    Inital version of mongo adminstrator

---
+++ app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var express     = require('express');
+var config      = require('./config');
+var middleware  = require('./middleware');
+
+var app = express();
+
+app.use('/', middleware(config));
+app.set('read_only', config.options.readOnly || false);
+
+var defaultPort = 80;
+var server      = app;
+
+if (config.site.sslEnabled){
+  defaultPort     = 443;
+  var https       = require('https');
+  var fs          = require('fs');
+  var sslOptions  = {
+    key:  fs.readFileSync(config.site.sslKey),
+    cert: fs.readFileSync(config.site.sslCert)
+  };
+  server          = https.createServer(sslOptions, app);
+}
+
+server.listen(config.site.port, config.site.host, function() {
+  console.log('Mongo Administrator server listening',
+      'on port ' + (config.site.port || defaultPort),
+      'at ' + (config.site.host || '0.0.0.0'));
+});
